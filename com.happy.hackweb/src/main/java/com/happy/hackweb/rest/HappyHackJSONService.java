6346a35908ec7d99e0a477e1d11aefb6c3133131
rajesharma470 <rajesharma470@gmail.com>
2015-04-11 19:04:45 +0530

    Added with storm fields

--- com.happy.hackweb/src/main/java/com/happy/hackweb/rest/HappyHackJSONService.java
+++ com.happy.hackweb/src/main/java/com/happy/hackweb/rest/HappyHackJSONService.java
@@ -1,6 +1,7 @@
 package com.happy.hackweb.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,21 +10,49 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
+import storm.starter.spout.InAppNotificationSpout;
+import storm.starter.spout.LineReaderSpout;
+import storm.starter.spout.NotificationBolt;
+import storm.starter.spout.WordCounterBolt;
+import storm.starter.spout.WordSpitterBolt;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+
 @Path("/activity")
 public class HappyHackJSONService {
-	
-	
-	  @POST
-	  @Consumes(MediaType.APPLICATION_JSON)
-	  public Response createXmlEventsInJSON(@Context Request request){
-		  
-		  System.out.println("request is==="+request);
-		  
-		  return Response.status(200).entity("").build();
-		  
-		  
-	  }
-	
-	
+
+	private static LocalCluster cluster = new LocalCluster();
+
+	private static void setTopologies() {
+		Config config = new Config();
+		config.setDebug(true);
+		config.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("inApp-spout", new InAppNotificationSpout());
+		builder.setBolt("inApp-bolt", new NotificationBolt()).shuffleGrouping("inApp-spout");
+		
+		
+		cluster.submitTopology("HelloStorm", config, builder.createTopology());
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createXmlEventsInJSON(@Context Request request) {
+
+		System.out.println("request is===" + request);
+
+		return Response.status(200).entity("").build();
+
+	}
+
+	@GET
+	public Response shutDown() {
+
+		System.out.println("Stopping the cluster");
+		cluster.shutdown();
+		return Response.status(200).entity("").build();
+	}
 
 }
